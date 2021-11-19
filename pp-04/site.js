7631d1807831b70d02ca67fcd5bb1687a58ae69c
Milan Debnath <mdebnath@hawk.iit.edu>
2021-11-18 23:47:09 -0600

    handle non-num with isNaN function

--- pp-04/site.js
+++ pp-04/site.js
@@ -23,7 +23,7 @@ console.log('The value of x is', x, '-- it should be 5.'); */
   Be sure to comment out the original `double()` function so you
   can accurately test your work.
 */
-x = 5;
+/*x = 5;
 
 var newFunc = {
   double: function double(num) {
@@ -32,10 +32,22 @@ var newFunc = {
 } };
 
 
-console.log(newFunc.double(6));
+console.log(newFunc.double(5));*/
 
 /*
   Step 3. Rewrite your corrected `double()` function from Step 2.
   so that non-number values passed into the function are handled
   in some reasonable way.
 */
+
+x = 5;
+
+var newFunc = {
+  double: function double(num) {
+    if (isNaN(num))
+      console.log("You have entered: " + num + " which is not a number");
+    var x = num * 2;
+  return x;
+} };
+
+console.log(newFunc.double("blah blah"));
